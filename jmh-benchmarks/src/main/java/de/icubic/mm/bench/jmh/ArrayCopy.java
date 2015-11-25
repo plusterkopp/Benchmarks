6d84447bf1a1fda61fb94a4a891f6cbf42b2c67c
plusterkopp <ralf@pkmd.de>
2015-11-25 02:25:21 +0100

    + ArrayCopy.java

--- jmh-benchmarks/src/main/java/de/icubic/mm/bench/jmh/ArrayCopy.java
+++ jmh-benchmarks/src/main/java/de/icubic/mm/bench/jmh/ArrayCopy.java
@@ -1,4 +1,4 @@
-package src.main.java.de.icubic.mm.bench.jmh;
+package de.icubic.mm.bench.jmh;
 
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.runner.*;
@@ -7,7 +7,8 @@ import org.openjdk.jmh.runner.options.*;
 @State(Scope.Benchmark)
 public class ArrayCopy {
 
-	private static final int ArraySize = 128 * 1024 * 1024;
+	private static final int Mega = 1024 * 1024;
+	private static final int ArraySize = 512 * Mega;
 	private static byte[] y;
 	private static byte[] x;
 
@@ -36,7 +37,8 @@ public class ArrayCopy {
         Options opt = new OptionsBuilder()
                 .include( ArrayCopy.class.getSimpleName())
 		        .warmupIterations(5)
-		        .measurementIterations(5)
+		        .measurementTime(TimeValue.seconds( 10))
+				.measurementIterations( 5)
 		        .forks(1)
                 .build();
         new Runner(opt).run();
