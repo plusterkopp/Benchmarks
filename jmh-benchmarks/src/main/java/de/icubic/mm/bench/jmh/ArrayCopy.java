9b155a51ce047bc87fd735c2f4126c01a09e6b9e
rhelbing <rhelbing@nbhelbing.icubic.icubic.de>
2015-11-25 01:29:12 +0100

    + ArrayCopy

---
+++ jmh-benchmarks/src/main/java/de/icubic/mm/bench/jmh/ArrayCopy.java
@@ -0,0 +1,44 @@
+package src.main.java.de.icubic.mm.bench.jmh;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.*;
+import org.openjdk.jmh.runner.options.*;
+
+@State(Scope.Benchmark)
+public class ArrayCopy {
+
+	private static final int ArraySize = 128 * 1024 * 1024;
+	private static byte[] y;
+	private static byte[] x;
+
+	@Setup(Level.Trial)
+	public static void setup() {
+		x = new byte[ ArraySize];
+		y = new byte[ ArraySize];
+	}
+
+	@Benchmark
+	@OperationsPerInvocation(ArraySize)
+	public void byteCopy() {
+		for ( int j = ArraySize - 1;  j >= 0; j--) {
+			y[ j] = x[ j];
+		}
+	}
+
+
+	@Benchmark
+	@OperationsPerInvocation(ArraySize)
+	public void arrayCopy() {
+		System.arraycopy( x, 0, y, 0, x.length);
+	}
+
+	public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include( ArrayCopy.class.getSimpleName())
+		        .warmupIterations(5)
+		        .measurementIterations(5)
+		        .forks(1)
+                .build();
+        new Runner(opt).run();
+    }
+}
