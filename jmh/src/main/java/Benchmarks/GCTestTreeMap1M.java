782c1e5f9547e05caad37c916d0bb01c8bab4081
Ralf H <ralf@pkmd.de>
2019-01-09 01:03:02 +0100

    + Added GCTestTreeMap1M

---
+++ jmh/src/main/java/Benchmarks/GCTestTreeMap1M.java
@@ -0,0 +1,95 @@
+package Benchmarks;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+import java.util.Random;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 1, time = 10)
+@Measurement(iterations = 4, time = 30)
+@Fork(1)
+@State(Scope.Benchmark)
+public class GCTestTreeMap1M {
+
+	@Param({
+			"10"
+			, "100"
+			, "1000"
+			, "10000"
+	})
+	int fillSize;
+
+    static class Container {
+        int    array[];
+
+        Container( int size) {
+        	array = new int[ size / 4];
+		}
+    }
+
+    SortedMap<Integer, Container> map = new TreeMap<>();
+	Random  rnd = new Random();
+
+	int	mapSize = 1_000_000;
+
+	@Setup
+    public void fillList() {
+        for ( int i = 0;  i < mapSize;  i++) {
+            map.put( i, new Container( fillSize));
+        }
+    }
+
+	private Container addRemove() {
+		int key = rnd.nextInt( mapSize);
+		Container c = new Container( fillSize);
+		c.array[ 0] = key;
+		Container container = map.put( key, c);
+		return container;
+	}
+
+	@Benchmark
+	@Fork(value = 1, jvmArgsPrepend = {"-XX:+UseParallelGC"})
+	public Container addRemovePS() {
+    	return addRemove();
+	}
+
+	@Benchmark
+	@Fork(value = 1, jvmArgsPrepend = {"-XX:+UseG1GC"})
+	public Container addRemoveG1() {
+		return addRemove();
+	}
+
+	@Benchmark
+	@Fork(value = 1, jvmArgsPrepend = {"-XX:+UseConcMarkSweepGC"})
+	public Container addRemoveCMS() {
+		return addRemove();
+	}
+
+	@Benchmark
+	@Fork(value = 1, jvmArgsPrepend = {"-XX:+UseShenandoahGC"})
+	public Container addRemoveShen() {
+		return addRemove();
+	}
+
+	public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include( GCTestTreeMap1M.class.getSimpleName())
+                .warmupIterations(3)
+                .measurementTime(TimeValue.seconds( 10))
+                .measurementIterations( 5)
+//                .timeUnit( TimeUnit.NANOSECONDS)
+                .forks(1)
+                .build();
+        new Runner(opt).run();
+    }
+
+}
