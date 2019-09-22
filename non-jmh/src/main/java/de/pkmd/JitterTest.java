d47da8ab885e79a5a91fb31713410438df2948dd
Ralf H <ralf@pkmd.de>
2019-09-22 23:43:36 +0200

    Changed: JitterTest

--- non-jmh/src/main/java/de/pkmd/JitterTest.java
+++ non-jmh/src/main/java/de/pkmd/JitterTest.java
@@ -15,19 +15,25 @@ public class JitterTest {
 		long lastA[] = { 0};
 		long intervalNS = 10_000_000;
 		long delay = 10_000;
+		ScheduledFuture f[] = { null};
+		StringBuilder   sb = new StringBuilder();
 		Runnable task = () -> {
+			long d = f[ 0].getDelay( TimeUnit.NANOSECONDS);
 			long now = System.nanoTime();
 			long sinceThenNS = now - thenA[ 0];
 			int count = countA[ 0];
 			long sinceThenExpectedNS = delay + intervalNS * count;
 			long diffNS = sinceThenNS - sinceThenExpectedNS;
 			long sinceLastNS = now - lastA[0];
-			System.out.println( "round: " + count
-					+ " later by: " + nf.format( diffNS)
+			sb.append( "round: " + count
+					+ " late by: " + nf.format( diffNS)
 					+ " since last: " + nf.format(sinceLastNS)
 					+ " jitter since last: " + nf.format( sinceLastNS - intervalNS)
+					+ " job delay: " + nf.format( d)
+					+ "\n"
 			);
-			if ( sinceThenNS > 1_000_000_000) {
+			if ( sinceThenNS > 50_000_000_000L) {
+				System.out.println( sb.toString());
 				System.exit( 0);
 			}
 			countA[ 0]++;
@@ -35,6 +41,6 @@ public class JitterTest {
 		};
 		thenA[ 0] = System.nanoTime();
 		lastA[ 0] = thenA[ 0];
-		ses.scheduleAtFixedRate( task, delay, intervalNS, TimeUnit.NANOSECONDS);
+		f[ 0] = ses.scheduleAtFixedRate( task, delay, intervalNS, TimeUnit.NANOSECONDS);
 	}
 }
