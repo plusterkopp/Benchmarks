6c74d09fbd78fd6bd3b82bea82c4de5594e1094f
Ralf H <ralf@pkmd.de>
2019-11-03 01:21:17 +0100

    Added: ByteCodeBench

---
+++ jmh/src/main/java/perf/ByteCodeBench.java
@@ -0,0 +1,58 @@
+package perf;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.*;
+import org.openjdk.jmh.runner.options.*;
+
+import java.util.concurrent.*;
+
+@Warmup(iterations = 5, time = 4, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 10, timeUnit = TimeUnit.SECONDS)
+@Fork(value = 1)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+public class ByteCodeBench {
+
+	@Benchmark
+	public int simpleHS() {
+		return Thread.currentThread().getName().length();
+	}
+
+	@Benchmark
+	public int verboseHS() {
+		Thread thread = Thread.currentThread();
+		String name = thread.getName();
+		int length = name.length();
+		return length;
+	}
+
+	@Benchmark
+	@Fork( jvmArgsPrepend = {"-XX:+UnlockExperimentalVMOptions", "-XX:+EnableJVMCI", "-XX:+UseJVMCICompiler"})
+	public int simpleGraal() {
+		return Thread.currentThread().getName().length();
+	}
+
+	@Benchmark
+	@Fork( jvmArgsPrepend = {"-XX:+UnlockExperimentalVMOptions", "-XX:+EnableJVMCI", "-XX:+UseJVMCICompiler"})
+	public int verboseGraal() {
+		Thread thread = Thread.currentThread();
+		String name = thread.getName();
+		int length = name.length();
+		return length;
+	}
+
+	public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include( ByteCodeBench.class.getSimpleName())
+		        .mode( Mode.AverageTime)
+		        .timeUnit(TimeUnit.NANOSECONDS)
+		        .warmupIterations(1)
+				.warmupTime( TimeValue.seconds( 1))
+		        .measurementIterations(5)
+				.measurementTime( TimeValue.seconds( 5))
+		        .forks(1)
+                .build();
+        new Runner(opt).run();
+    }
+}
