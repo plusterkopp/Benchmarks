a5a7439c42555709710555c82131cd0e8e86ddfb
Ralf H <ralf@pkmd.de>
2020-12-30 01:45:43 +0100

    Added: StringInternBench multithread

--- jmh/src/main/java/Benchmarks/StringInternBench.java
+++ jmh/src/main/java/Benchmarks/StringInternBench.java
@@ -6,6 +6,7 @@ import org.openjdk.jmh.runner.options.*;
 
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.concurrent.locks.*;
 
 @Warmup(iterations = 5, time = 4, timeUnit = TimeUnit.SECONDS)
 @Measurement(iterations = 5, time = 10, timeUnit = TimeUnit.SECONDS)
@@ -15,21 +16,29 @@ import java.util.concurrent.*;
 @State(Scope.Benchmark)
 public class StringInternBench {
 
-	@Param( { "10000", "100000", "1000000", "10000000"}) // "100", "1000", , "100000", "1000000"
+	@Param( { "10000", "100000", "1000000"}) // "100", "1000", , "100000", "1000000"
 	int uniqueCount;
 
 	final int stringCount = 1_000_000;
 	List<String>  stringList = new ArrayList<String>( stringCount);
 	int runCount = 1;
+	ConcurrentHashMap<String, String> chm;
+	ReadWriteLock   lock = new ReentrantReadWriteLock();
 
 	@Setup(Level.Invocation)
 	public void setup() {
-		stringList.clear();
-		for (int i = 0; i < stringCount; i++) {
-			long value = i % uniqueCount;
-			stringList.add( Integer.toString( runCount) + "-" + Long.toString( value));
+		lock.writeLock().lock();
+		try {
+			chm = new ConcurrentHashMap<>();
+			stringList.clear();
+			for (int i = 0; i < stringCount; i++) {
+				long value = i % uniqueCount;
+				stringList.add(Integer.toString(runCount) + "-" + Long.toString(value));
+			}
+			runCount++;
+		} finally {
+			lock.writeLock().unlock();
 		}
-		runCount++;
 	}
 
 	@Benchmark
@@ -38,20 +47,57 @@ public class StringInternBench {
 		for (int i = 0; i < stringList.size(); i++) {
 			String s = stringList.get( i);
 			String intern = s.intern();
-			stringList.set( i, intern);
+//			if ( s != intern) {
+//				stringList.set(i, intern);
+//			}
+		}
+	}
+
+	@Benchmark
+	@OperationsPerInvocation( stringCount)
+	@Threads( 4)
+	public void internString4T() {
+		lock.readLock().lock();
+		try {
+			for (int i = 0; i < stringList.size(); i++) {
+				String s = stringList.get( i);
+				String intern = s.intern();
+//			if ( s != intern) {
+//				stringList.set(i, intern);
+//			}
+			}
+		} finally {
+			lock.readLock().unlock();
 		}
 	}
 
 	@Benchmark
 	@OperationsPerInvocation( stringCount)
 	public void internCHM() {
-		ConcurrentHashMap<String, String> chm = new ConcurrentHashMap<>();
 		for (int i = 0; i < stringList.size(); i++) {
 			String s = stringList.get( i);
 			String present = chm.putIfAbsent(s, s);
-			if (present != null) {
-				stringList.set( i, present);
+//			if (present != null) {
+//				stringList.set( i, present);
+//			}
+		}
+	}
+
+	@Benchmark
+	@OperationsPerInvocation( stringCount)
+	@Threads( 4)
+	public void internCHM4T() {
+		lock.readLock().lock();
+		try {
+			for (int i = 0; i < stringList.size(); i++) {
+				String s = stringList.get( i);
+				String present = chm.putIfAbsent(s, s);
+//			if (present != null) {
+//				stringList.set( i, present);
+//			}
 			}
+		} finally {
+			lock.readLock().unlock();
 		}
 	}
 
